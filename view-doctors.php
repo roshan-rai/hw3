582a4ad348b84f5423c3a1363addb7c10fc0e7e7
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-09-27 14:19:47 -0500

    Update view-doctors.php

--- view-doctors.php
+++ view-doctors.php
@@ -16,12 +16,12 @@
               {
                 ?>
                   <tr>
-                    <td><?php echo $doctor['doctors_id];?></td>
+                    <td><?php echo $doctor['doctors_id'];?></td>
                     <td>
-                      <?php echo $doctor['doctors_name];?></td>
+                      <?php echo $doctor['doctors_name'];?></td>
                     </td>
                     <td>
-                      <?php echo $doctor['doctors_area];?></td>
+                      <?php echo $doctor['doctors_area'];?></td>
                     </td>
                   </tr>
                   
