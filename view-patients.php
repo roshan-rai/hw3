e5287728b8805845584def748a6c70af49f06741
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-09-27 14:35:35 -0500

    Update view-patients.php

--- view-patients.php
+++ view-patients.php
@@ -1,1 +1,32 @@
-
+<h1>Patients</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+         <th>Patient ID</th>
+      <th>Patient Name</th>
+      <th>Description</th>
+  
+      </tr>
+     
+    </thead>
+      <tbody>
+        <?php
+            while ($patient = $patients-> fetch_assoc())
+              {
+                ?>
+                  <tr>
+                    <td><?php echo $patient['patient_id'];?></td>
+                    <td>
+                      <?php echo $$patient['patient_name'];?></td>
+                    </td>
+                    <td>
+                      <?php echo $$patient['patient_description'];?></td>
+                    </td>
+                  </tr>                  
+                <?php
+              }
+        ?>
+      </tbody>    
+  </table>
+</div>
