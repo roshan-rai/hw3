e91d16af64cb9271c8a12f3e76a05bd3da806bb7
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-09-27 14:51:32 -0500

    Update model-patients.php

--- model-patients.php
+++ model-patients.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectPatients() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT patient_id, patient_name, patient_description FROM `patient` ");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
