b731f8bd3cc90981dc01c2adf0ddc2b9e2b23aa1
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-09-27 14:30:41 -0500

    Update patients.php

--- patients.php
+++ patients.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-patients.php");
 
+$pageTitle = "Patients";
+include "view-header.php";
+$patients = selectPatients();
+include "view-patients.php";
+include "view-footer.php";
+?>
