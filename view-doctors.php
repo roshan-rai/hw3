0a9cc8ae70fe0eb7eca46a54829be911c3d8c7df
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-09-27 00:22:30 -0500

    Create view-doctors.php

---
+++ view-doctors.php
@@ -0,0 +1,34 @@
+<h1>Doctors</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+         <th>ID</th>
+      <th>Name</th>
+      <th>Area</th>
+  
+      </tr>
+     
+    </thead>
+      <tbody>
+        <?php>
+            while($doctor = $doctors-> fetch_assoc())
+              {
+                ?>
+                  <tr>
+                    <td><?php echo $doctor['doctors_id];?></td>
+                    <td>
+                      <?php echo $doctor['doctors_name];?></td>
+                    </td>
+                    <td>
+                      <?php echo $doctor['doctors_area];?></td>
+                    </td>
+                  </tr>
+                  
+                <?php
+              }
+        <?>
+      </tbody>
+    
+  </table>
+</div>
