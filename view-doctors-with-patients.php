41c44e03766f085f27ed11506d6f120d30bb85e6
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-09-27 20:44:27 -0500

    Create view-doctors-with-patients.php

---
+++ view-doctors-with-patients.php
@@ -0,0 +1,21 @@
+<h1>Doctors with Patients</h1>
+<div class="card-group">
+        <?php
+            while ($doctor = $doctors-> fetch_assoc())
+              {
+                ?>
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title"><?php echo $doctor['doctor_name'];?></h5>
+                      <p class="card-text">
+                        <?php
+                          $patients=selectPatientsByDoctor($doctor['doctor_id'];);
+                        ?>
+                      </p>
+                      <p class="card-text"><small class="text-body-secondary">Specialist:  <?php echo $doctor['doctor_area'];?></small></p>
+                    </div>
+                    </div>             
+                <?php
+              }
+        ?>
+    </div>   
