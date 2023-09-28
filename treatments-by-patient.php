149bfbc6fbb59d1d88a46d06d012e9e672225b11
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-09-27 20:23:43 -0500

    Update and rename treatment-by-patient.php to treatments-by-patient.php

--- treatments-by-patient.php
+++ treatments-by-patient.php
@@ -2,7 +2,7 @@
 require_once("util-db.php");
 require_once("model-treatments-by-patient.php");
 
-$pageTitle = "Treatment by Patients";
+$pageTitle = "Treatments by Patient";
 include "view-header.php";
 $treatments = selectTreatmentsByPatient($_POST['pid']);
 include "view-treatments-by-patient.php";
