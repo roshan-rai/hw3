c9a619f606bf3f67b38d5e8c95de89334ff8c480
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-09-27 19:48:18 -0500

    Update and rename model-courses-by-instructor.php to model-patients-by-doctor.php

--- model-patients-by-doctor.php
+++ model-patients-by-doctor.php
@@ -1,5 +1,5 @@
 <?php
-function selectPatientsByInstructor($did) {
+function selectPatientsByDoctor($did) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT p.patient_id, patient_name, patient_description,treatment_name, treatment_startdate, treatment_enddate FROM `patient` p join treatment t on t.doctor_id=p.patient_id WHERE t.doctor_id=? ");
