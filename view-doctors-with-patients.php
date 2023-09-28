512e699a0e93d7757bb911a08759fdb0b0db7a49
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-09-27 20:47:06 -0500

    Update view-doctors-with-patients.php

--- view-doctors-with-patients.php
+++ view-doctors-with-patients.php
@@ -9,7 +9,13 @@
                       <h5 class="card-title"><?php echo $doctor['doctor_name'];?></h5>
                       <p class="card-text">
                         <?php
-                          $patients=selectPatientsByDoctor($doctor['doctor_id'];);
+                          $patients=selectPatientsByDoctor($doctor['doctor_id']);
+                              while ($patient=$patients->fetch_assoc())
+                                      {
+                                            ?>
+                                              
+                                         <?php
+                                      }
                         ?>
                       </p>
                       <p class="card-text"><small class="text-body-secondary">Specialist:  <?php echo $doctor['doctor_area'];?></small></p>
