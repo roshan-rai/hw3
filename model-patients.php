8f6616807f975d6a56deb906931d79dee2e6f55b
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-10-18 10:56:47 -0500

    Update model-patients.php
    changed the CRUD back undid it because i accidently did on the hw3 main branch

--- model-patients.php
+++ model-patients.php
@@ -13,45 +13,5 @@ function selectPatients() {
     }
 }
 
-function insertPatients($pName, $pDescription) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("INSERT INTO `patient` (`patient_name`, `patient_description`) VALUES (?,?)");
-        $stmt->bind_param("ss", $pName, $pDescription);
-        $success = $stmt->execute();
-        $conn->close();
-        return $success;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
 
-function updatePatients($pName, $pDescription, $pid) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("update `patient`set `patient_name`=?, `patient_description` = ? where patient_id=?");
-        $stmt->bind_param("ssi", $pName, $pDescription, $pid);
-        $success = $stmt->execute();
-        $conn->close();
-        return $success;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
-
-function deletePatients($pid) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("delete from patient where patient_id=?");
-        $stmt->bind_param("i", $pid);
-        $success = $stmt->execute();
-        $conn->close();
-        return $success;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
 ?>
