b1b5beb957cc2e3640aca25a545b7c6dc1434c2f
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-10-18 15:53:33 -0500

    Update patients.php
    added the if and switch in patients.php

--- patients.php
+++ patients.php
@@ -4,6 +4,15 @@ require_once("model-patients.php");
 
 $pageTitle = "Patients";
 include "view-header.php";
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      insertCourse($_POST['pName'], $_POST['pDescription']);
+      break;
+    // Add more cases here if needed
+  }
+}
+
 $patients = selectPatients();
 include "view-patients.php";
 include "view-footer.php";
