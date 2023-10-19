8b394586c6332fddb6fa9094b2c6a00e5755f08a
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-10-18 19:15:49 -0500

    Update model-doctors-with-patients.php

--- model-doctors-with-patients.php
+++ model-doctors-with-patients.php
@@ -16,7 +16,7 @@ function selectDoctors() {
 function selectPatientsByDoctor($did) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT p.patient_id, patient_name, patient_description,treatment_name, treatment_startdate, treatment_enddate FROM patient p join treatment t on t.doctor_id=p.patient_id WHERE t.doctor_id=? ");
+        $stmt = $conn->prepare("SELECT p.patient_id, patient_name, patient_description, treatment_name, treatment_startdate, treatment_enddate FROM patient p JOIN treatment t ON t.patient_id = p.patient_id WHERE t.doctor_id = ?");
         $stmt->bind_param("i", $did);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -27,5 +27,35 @@ function selectPatientsByDoctor($did) {
         throw $e;
     }
 }
+
+
+function selectDoctorsForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT doctor_id, doctor_name FROM `doctor` order by doctor_name ");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectPatientForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT patient_id, patient_name FROM `patient` order by course_name ");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 
 ?>
