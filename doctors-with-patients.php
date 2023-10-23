ae11b050654734e4679ac9c2c8c08603d2350a12
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-10-22 21:08:08 -0500

    Update doctors-with-patients.php

--- doctors-with-patients.php
+++ doctors-with-patients.php
@@ -4,9 +4,39 @@ require_once("model-doctors-with-patients.php");
 
 $pageTitle = "Doctors";
 include "view-header.php";
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+        if(insertDoctorWithPatients( $_POST['did'], $_POST['pid'], $_POST['treatment_name'], $_POST['sDate'], $_POST['eDate']) )
+      {
+        echo '<div class="alert alert-success" role="alert"> Patients successfully added!</div>';
+      }
+      else
+      {
+       echo '<div class="alert alert-danger" role="alert"> Error</div>';
+      }
+      break;
+    case "Edit":
+      if(updatePatients($_POST['pName'], $_POST['pDescription'], $_POST['pid']))
+      {
+        echo '<div class="alert alert-success" role="alert"> Patients successfully edited!</div>';
+      }
+      else
+      {
+       echo '<div class="alert alert-danger" role="alert"> Error</div>';
+      }
+      break;
+        case "Delete":
+      if (deletePatients($_POST['pid'])) 
+      {
+            echo '<div class="alert alert-success" role="alert"> Patient successfully deleted!</div>';
+        } 
+      else {
+    echo '<div class="alert alert-danger" role="alert"> Error</div>';
+        }  
+
 $doctors = selectDoctors();
 
-insertDoctorWithPatients( $did, $pid, $treatment_name, $sDate, $eDate) 
 include "view-doctors-with-patients.php";
 include "view-footer.php";
 ?>
