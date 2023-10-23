fc6dc44800be11d52b2bf978171c614e5def58df
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-10-22 20:26:27 -0500

    Update model-doctors-with-patients.php

--- model-doctors-with-patients.php
+++ model-doctors-with-patients.php
@@ -56,6 +56,18 @@ function selectPatientForInput() {
         throw $e;
     }
 }
-
+function deleteDoctorWithPatients() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT doctor_id, doctor_name, doctor_area FROM `doctor` ");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 
 ?>
