f7f7e888d11daa7ab162313f2c5e1cca92e9e4d5
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-13 15:34:31 -0600

    Update index.php
    checking

--- index.php
+++ index.php
@@ -1,45 +1,69 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"></script>
+<h1 class="ml1">
+  <span class="text-wrapper">
+    <span class="line line1"></span>
+    <span class="letters">THURSDAY</span>
+    <span class="line line2"></span>
+  </span>
+</h1>
 
-<?php
-$pageTitle = "Home";
-include "view-header.php";
-?>
-<div class="grid">
-  <div class="item">Item 1</div>
-  <div class="item">Item 2</div>
-  <div class="item">Item 3</div>
-  <!-- Add more items as needed -->
-</div>
-<h1>Homework 6</h1>
-<?php
-include "view-footer.php";
-?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
 
-<script>
-  const grid = document.querySelector('.grid')
-  const items = document.querySelectorAll('.item')
+.ml1 {
+  font-weight: 900;
+  font-size: 3.5em;
+}
 
-  items.forEach((item) => {
-    item.addEventListener('mouseenter', () => {
-      gsap.to(grid, {
-        '--track': '2fr',
-        duration: 0.3,
-      })
-      gsap.to(item, {
-        '--innerTrack': '1fr',
-        duration: 0.3,
-      })
-    })
+.ml1 .letter {
+  display: inline-block;
+  line-height: 1em;
+}
 
-    item.addEventListener('mouseleave', () => {
-      gsap.to(grid, {
-        '--track': '1fr',
-        duration: 0.3,
-      })
-      gsap.to(item, {
-        '--innerTrack': '0fr',
-        duration: 0.3,
-      })
-    })
-  })
-</script>
+.ml1 .text-wrapper {
+  position: relative;
+  display: inline-block;
+  padding-top: 0.1em;
+  padding-right: 0.05em;
+  padding-bottom: 0.15em;
+}
+
+.ml1 .line {
+  opacity: 0;
+  position: absolute;
+  left: 0;
+  height: 3px;
+  width: 100%;
+  background-color: #fff;
+  transform-origin: 0 0;
+}
+
+.ml1 .line1 { top: 0; }
+.ml1 .line2 { bottom: 0; }
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml1 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml1 .letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 600,
+    delay: (el, i) => 70 * (i+1)
+  }).add({
+    targets: '.ml1 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: '-=875',
+    delay: (el, i, l) => 80 * (l - i)
+  }).add({
+    targets: '.ml1',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
